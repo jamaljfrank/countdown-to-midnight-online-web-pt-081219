14e69f6eaf226c0dea3cb2c9cb66fa4628e89bf8
Jamal Frank <jamaljfrank@gmail.com>
2019-08-21 05:12:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,4 +11,10 @@ return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
